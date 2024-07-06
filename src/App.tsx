4ded6e99cb76b86a4a72295eefccbae9f682cf44
ValeriiaKuz <lerabukareva@icloud.com>
2024-07-06 15:23:51 +0300

    feat: add globals styles and favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import './App.css';
+import { Component } from 'react';
+import { MainPage } from './pages/Main/MainPage';
 
-function App() {
-  return <></>;
+class App extends Component {
+  render() {
+    return (
+      <>
+        <MainPage />
+      </>
+    );
+  }
 }
 
 export default App;
