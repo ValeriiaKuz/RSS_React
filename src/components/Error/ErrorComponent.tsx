f140c1c31493346951c002f8d75f3143b4d4e981
ValeriiaKuz <lerabukareva@icloud.com>
2024-07-14 21:16:06 +0300

    feat: change error component for routing

--- src/components/Error/ErrorComponent.tsx
+++ src/components/Error/ErrorComponent.tsx
@@ -1,15 +1,17 @@
 import { FC } from 'react';
 import styles from './Error.module.css';
+import { useRouteError } from 'react-router-dom';
 interface ErrorProps {
-  message: string;
+  message?: string;
 }
 export const ErrorComponent: FC<ErrorProps> = ({ message }) => {
+  const error = useRouteError() as Error;
   return (
     <div className={styles.error}>
       <p className={styles.error_message}>
         Something went wrong
         <br />
-        {message}
+        {message || error.message}
       </p>
     </div>
   );
